c610cc0d12171a513239dc0674234c2a87e1b5e9
Gülhan Tezcan <gulhante@gmail.com>
2023-08-13 12:54:56 +0200

    parallel test hinzugefügt

--- src/test/java/pages/parallelTestwithLocalDriver/AmazonBasePage.java
+++ src/test/java/pages/parallelTestwithLocalDriver/AmazonBasePage.java
@@ -6,54 +6,55 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
+import utilities.WebDriverFactoryStaticThreadLocal;
 
 public class AmazonBasePage {
 
 
     // Kategorien-Tab "Computer" auswählen
     public void selectDropDown() {
-        WebElement searchDropdown = Driver.getDriver().findElement(By.id("searchDropdownBox"));
+        WebElement searchDropdown = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.id("searchDropdownBox"));
         Select select = new Select(searchDropdown);
         select.selectByVisibleText("Computer");
     }
 
     // Computer-Kategorie ausgewählt und überprüft
     public String selectedCategoryVerify() {
-        WebElement searchDropdown = Driver.getDriver().findElement(By.id("searchDropdownBox"));
+        WebElement searchDropdown = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.id("searchDropdownBox"));
         return searchDropdown.getText();
     }
 
     // In die Suchleiste "msi" eingeben und Suche ausführen
     public void searchArea(String searchWord) {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys(searchWord + Keys.ENTER);
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys(searchWord + Keys.ENTER);
     }
 
 
     public void cartClick() {
-        Driver.getDriver().findElement(By.className("nav-cart-icon nav-sprite")).click();
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.className("nav-cart-icon nav-sprite")).click();
     }
 
 
     // Login click
     public void loginClick() {
-        Driver.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']")).click();
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']")).click();
     }
 
 
 
     // Login-Seite wird geöffnet und überprüft
     public WebElement loginVerify() {
-        WebElement loginVerify = Driver.getDriver().findElement(By.id("nav-link-accountList-nav-line-1"));
+        WebElement loginVerify = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.id("nav-link-accountList-nav-line-1"));
         return loginVerify;
     }
 
 
     // Melden Sie sich ab.
     public void abmelden() {
-        WebElement abmelden = Driver.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']"));
-        Actions actions = new Actions(Driver.getDriver());
+        WebElement abmelden = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//a[@data-nav-role='signin']"));
+        Actions actions = new Actions(WebDriverFactoryStaticThreadLocal.getDriver());
         actions.moveToElement(abmelden).perform();
-        Driver.getDriver().findElement(By.xpath("//span[.='Abmelden']")).click();
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//span[.='Abmelden']")).click();
     }
 }
 
