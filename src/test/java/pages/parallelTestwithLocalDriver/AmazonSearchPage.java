05fae7958fc627dd9fe6147ab5535d30099ca9a0
Gülhan Tezcan <gulhante@gmail.com>
2023-08-13 12:56:16 +0200

    parallel test hinzugefügt

--- src/test/java/pages/parallelTestwithLocalDriver/AmazonSearchPage.java
+++ src/test/java/pages/parallelTestwithLocalDriver/AmazonSearchPage.java
@@ -3,6 +3,7 @@ package pages.parallelTestwithLocalDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utilities.Driver;
+import utilities.WebDriverFactoryStaticThreadLocal;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,48 +13,48 @@ public class AmazonSearchPage extends AmazonBasePage {
 
     // Die Suchergebnisseite wird geöffnet und überprüft
     public String searchText() {
-        WebElement searchText = Driver.getDriver().findElement(By.xpath("//span[@class='a-color-state a-text-bold']"));
+        WebElement searchText = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//span[@class='a-color-state a-text-bold']"));
        return searchText.getText();
     }
 
 
     // Auf die 2. Seite der Suchergebnisse navigieren
     public void seiteZweiClick(){
-        WebElement seiteZwei = Driver.getDriver().findElement(By.xpath("//a[@aria-label='Zu Seite 2']"));
+        WebElement seiteZwei = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//a[@aria-label='Zu Seite 2']"));
         seiteZwei.click();
     }
 
 
     // 2. Seite wird geöffnet und überprüft
     public WebElement seiteZweiVerify(){
-        WebElement seiteZweiVerify = Driver.getDriver().findElement(By.xpath("//span[@aria-label='Aktuelle Seite, Seite 2']"));
+        WebElement seiteZweiVerify = WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//span[@aria-label='Aktuelle Seite, Seite 2']"));
         return seiteZweiVerify;
     }
 
 
     // Wählen Sie das zweite Produkt aus
     public WebElement products(){
-        List<WebElement> productsList = new ArrayList<>(Driver.getDriver().findElements(By.className("s-image")));
+        List<WebElement> productsList = new ArrayList<>(WebDriverFactoryStaticThreadLocal.getDriver().findElements(By.className("s-image")));
         return productsList.get(1);
     }
 
 
     // Das zweite Produkt auf der Seite wird dem Warenkorb hinzugefügt.
     public void addtoCart() {
-        Driver.getDriver().findElement(By.xpath("(//input[@id='add-to-cart-button'])[1]")).click();
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("(//input[@id='add-to-cart-button'])[1]")).click();
     }
 
 
 
     // Überprüfen Sie, ob das Produkt zum Warenkorb hinzugefügt wurde.
     public String hinzufugen(){
-        return  Driver.getDriver().findElement(By.xpath("//*[@id=\"NATC_SMART_WAGON_CONF_MSG_SUCCESS\"]/span")).getText();
+        return  WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//*[@id=\"NATC_SMART_WAGON_CONF_MSG_SUCCESS\"]/span")).getText();
     }
 
 
     // Überprüfen Sie, ob die Warenkorbseite geöffnet ist.
     public void einkaufswagwen(){
-        Driver.getDriver().findElement(By.xpath("//*[@id='sw-gtc']/span/a")).click();
+        WebDriverFactoryStaticThreadLocal.getDriver().findElement(By.xpath("//*[@id='sw-gtc']/span/a")).click();
     }
 
 }
